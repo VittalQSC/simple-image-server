3f36955d5036cc27eb1013a2ecc472baa55478f9
vitali_shatsou <vitali_shatsou@epam.com>
2020-03-16 10:46:15 +0100

    add file-names recursive

--- index.js
+++ index.js
@@ -6,6 +6,30 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const dir = path.join(__dirname, 'public');
 
+var walk = function(dir, done) {
+    var results = [];
+    fs.readdir(dir, function(err, list) {
+      if (err) return done(err);
+      var i = 0;
+      (function next() {
+        var file = list[i++];
+        if (!file) return done(null, results);
+        file = path.resolve(dir, file);
+        fs.stat(file, function(err, stat) {
+          if (stat && stat.isDirectory()) {
+            walk(file, function(err, res) {
+              results = results.concat(res);
+              next();
+            });
+          } else {
+            results.push(file);
+            next();
+          }
+        });
+      })();
+    });
+  };
+
 app.use(express.static(dir));
 
 app.get('/', function(req, res) {
@@ -13,13 +37,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/file-names', function(req, res) {
-    fs.readdir(dir, function(err, files) {
+    walk(dir, function(err, results) {
         if (err) {
-            console.log('ERROR', err);
+            console.log('ERROE', err);
             return;
-        }
-
-        res.send(files);
+        };
+        res.send(results.map(result => result.replace(dir, '')));
     });
 });
 
